bcec378b09ff8a699fdda5a7c5b292a27273a271
Jason Baker <jwbaker@uwaterloo.ca>
2016-08-10 16:57:50 -0400

    Basic outline of the application; no logic yet

---
+++ server.js
@@ -0,0 +1,18 @@
+// Borrowed from http://scotch.io.com/tutorials/how-to-deploy-a-node-js-app-to-heroku
+
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.listen(port, function(){
+	console.log('Listening on http://localhost:' + port);
+});
