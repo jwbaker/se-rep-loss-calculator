e880b9b1fcf82c82d0b128b13cd249209ec65494
Jason Baker <jwbaker@uwaterloo.ca>
2016-08-10 18:08:36 -0400

    Added some authentication logic

---
+++ public/js/se-integration.js
@@ -0,0 +1,46 @@
+var url = 'http://localhost:8000'
+
+var noLoginTemplate = '<button class="btn btn-primary">Log in</button>';
+var loginTemplate = '<p><img src="http://se-flair.appspot.com/__SEID__.png" alt=""></p>'
+
+var access_token = undefined;
+var users = undefined;
+
+$(document).ready(function(){
+	$('#login-container').html(noLoginTemplate);
+
+	SE.init({
+		clientId: 6388,
+		key: 'LatL0Qaw3)dPKFoK6Y1yiQ((',
+		channelUrl: url + '/blank',
+		complete: function(data){
+			console.log(data);
+		}
+	});
+
+	$('#login-container button').click(function(event){
+		SE.authenticate({
+			success: onLoginSuccess,
+			error: function(data){
+				alert('Something went wrong. Unable to log in');
+				console.log(data);
+			},
+			scope: ['private_info'],
+			networkUsers: true
+		});
+	});
+});
+
+var onLoginSuccess = function(data){
+	access_token = data.accessToken;
+	users = data.network_users;
+
+	for(var i = 0; i < users.length; i++){
+		if(users[i].site_url === 'http://stackexchange.com'){
+			$('#login-container').html(loginTemplate.replace(
+				'__SEID__',
+				users[i].account_id
+			));
+		}
+	}
+}
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 // Borrowed from http://scotch.io.com/tutorials/how-to-deploy-a-node-js-app-to-heroku
 
 var express = require('express');
+var moment = require('moment');
+
 var app = express();
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8000;
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-	res.render('index');
+	res.render('index', {
+		'year': moment().format('YYYY')
+	});
+});
+
+app.get('/blank', function(req, res){
+	res.render('blank');
 });
 
 app.listen(port, function(){
