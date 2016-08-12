2d328b04e18c398c3d803b3db392365438d79959
Jason Baker <jwbaker@uwaterloo.ca>
2016-08-12 11:08:38 -0400

    Finished off first iteration of rep calculation

--- public/js/se-integration.js
+++ public/js/se-integration.js
@@ -17,6 +17,8 @@ var targetSiteUrl = undefined;
 var fromDate = undefined;
 var toDate = undefined;
 
+var reputationObjects = {};
+
 $(document).ready(function(){
 	$('#login-container').html(noLoginTemplate);
 
@@ -44,7 +46,8 @@ $(document).ready(function(){
 	});
 
 	$('#calculate-button').click(function(event){
-		doRepCalculation();
+		buildReputationObjects();
+		doReputationCalculation(reputationObjects);
 	});
 
 	$('#site-selector-container select').change(function(event){
@@ -129,20 +132,111 @@ var setTargetSite = function(idx){
 	targetUserId = sites[idx].user_id;
 }
 
-var doRepCalculation = function(){
-	var queryString = stackUrl + version + '/me/reputation-history-full?' + $.param({
+var buildRepQueryString = function(page){
+	return stackUrl + version + '/me/reputation-history-full?' + $.param({
 		key: apiKey,
 		site: targetSiteUrl,
 		access_token: accessToken,
 		fromdate: fromDate,
 		toDate: toDate,
+		page: page
 	});
+}
+
+var buildReputationObjects = function(page){
+	page = page || 1;
+	var queryString = buildRepQueryString(page);
 
 	$.ajax(queryString)
 	.done(function(data, status, xhr){
 		console.log(data);
+		$.each(data.items, function(idx, elem){
+			if(!(elem.post_id in reputationObjects)){
+				reputationObjects[elem.post_id] = [];
+			}
+			reputationObjects[elem.post_id].push(elem);
+		});
+
+		if(data.has_more){
+			buildReputationObjects(page++);
+		}
 	})
 	.fail(function(xhr, status, err){
-		console.log(err);
+		alert('Something went wrong: ' + err);
 	});
+}
+
+var POST_TYPE_DEPENDENT = ['voter_downvotes', 'voter_undownvotes', 'post_upvoted', 'post_unupvoted'];
+var idsToCheck = {};
+
+var doReputationCalculation = function(postRepChanges){
+	var earnedRep = 0;
+	var actualRep = 0;
+
+	for(var postId in postRepChanges){
+		for(var i = 0; i < postRepChanges[postId].length; i++){
+			var repObject = postRepChanges[postId][i];
+			actualRep += repObject.reputation_change;
+
+			if($.inArray(repObject.reputation_history_type, POST_TYPE_DEPENDENT)){
+				if(!(postId in idsToCheck)){
+					idsToCheck[postId] = [];
+				}
+				idsToCheck[postId].push(repObject.reputation_history_type);
+			} else{
+				earnedRep += repObject.reputation_change;
+			}
+		}
+	}
+
+	earnedRep += doCalculateRepForPostTypes(idsToCheck);
+	alert("Earned: " + earnedRep + "; actual: " + actualRep);
+}
+
+var buildPostsQueryString = function(postIds){
+	var vectorizedPostIds = postIds.join(';');
+	return stackUrl + version + '/posts/' + vectorizedPostIds + '?' + $.param({
+		key: apiKey,
+		site: targetSiteUrl,
+		pagesize=100
+}
+
+var POST_DEPENDENT_REP_CHANGES = {
+	'question': {
+		'voter_downvotes': 0,
+		'voter_undownvotes': 0,
+		'post_upvoted': 5,
+		'post_unupvoted': -5
+	},
+	'answer': {
+		'voter_downvotes': -1,
+		'voter_undownvotes': 1,
+		'post_upvoted': 10,
+		'post_unupvoted': -10
+	}
+};
+
+var doCalculateRepForPostTypes = function(repForPostIds){
+	total = 0;
+	postIds = repForPostIds.keys();
+
+	for(var i = 0; i < postIds.length; i += 100){
+		var end = Math.min(postIds.length, i+100);
+		var queryIds = postIds.slice(i, end);
+
+		var queryString = buildPostsQueryString(queryIds);
+		$.ajax(queryString)
+		.done(function(data, status, xhr){
+			$.each(data.items, function(idx, elem){
+				var postType = elem.post_type;
+				for(var j = 0; j < repForPostIds[elem.post_id]; j++){
+					var repChangeType = repForPostIds[elem.post_id][j];
+					total += POST_DEPENDENT_REP_CHANGES[postType][repChangeType];
+				}
+			})
+		})
+		.fail(function(xhr, status, err){
+			alert('Something went wrong: ' + err);
+		});
+	}
 }
